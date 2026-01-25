0aace16aa4ed71b0f721e560170dbc311428118b
Niko Matsakis <niko@alum.mit.edu>
2026-01-25 11:38:45 -0500

    fix: use generated docs

--- src/battery-pack/templates/default/src/lib.rs
+++ src/battery-pack/templates/default/src/lib.rs
@@ -1,20 +1,3 @@
-//! {{project-name}}: A curated battery pack.
-//!
-//! This battery pack provides a curated set of crates for ...
-//!
-//! # Usage
-//!
-//! Add to your `Cargo.toml`:
-//!
-//! ```toml
-//! [dependencies]
-//! {{project-name}} = "0.1"
-//! ```
-//!
-//! Then use the re-exported crates:
-//!
-//! ```rust,ignore
-//! use {{crate_name}}::*;
-//! ```
+#![doc = include_str!(concat!(env!("OUT_DIR"), "/docs.md"))]
 
 include!(concat!(env!("OUT_DIR"), "/facade.rs"));
