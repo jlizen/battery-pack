cfe60083d771ce7b14139c22141ec864d9a40cc2
jlizen <jlizen@amazon.com>
2026-02-28 20:12:44 +0000

    refactor: update error-battery-pack to build with current codebase

--- src/error-battery-pack/src/lib.rs
+++ src/error-battery-pack/src/lib.rs
@@ -11,14 +11,3 @@ const SELF_MANIFEST: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/C
 pub fn validate() {
     battery_pack::validate(SELF_MANIFEST);
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn battery_pack_has_no_regular_dependencies() {
-        battery_pack::assert_no_regular_deps(include_str!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/Cargo.toml"
-        )));
-    }
-}
